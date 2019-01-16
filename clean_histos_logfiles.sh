51f4f49bf3efe90574699c735061bfc3f1423803
Hendrik Poppenborg <hendrik.poppenborg@cern.ch>
2019-01-16 16:45:56 +0100

    restrict cleaning script to jp directories

--- clean_histos_logfiles.sh
+++ clean_histos_logfiles.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-echo removing root files
-rm */*/*.root &
+echo 'removing root files'
+rm jp*/*/*.root &
 
-echo removing .exe files
-rm */working/*.exe &
+echo 'removing .exe files'
+rm jp*/working/*.exe &
 
-echo removing directories working/result*
-rm -rf */working/result*
+echo 'removing directories working/result*'
+rm -rf jp*/working/result*/ &
 
-echo removing directories *inclusive*
-rm -rf */*inclusive*
+echo 'removing directories *evts* (the single root files directories)'
+rm -rf jp*/*evts*/ &
 
-echo removing directories *LO*
-rm -rf */*LO*
+echo 'removing log files'
+rm jp*/working/log/* &
 
-echo removing log files
-rm */working/log/*
-
-echo removing *~
+echo 'removing *~'
 rm *~
 
 exit $?
